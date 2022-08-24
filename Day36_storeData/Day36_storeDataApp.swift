d94fa626a41558f0a9bb3b2ec468f29462d759f8
Raj <rajkhulna@outlook.com>
2022-08-24 14:47:57 +0600

    Initial Commit

---
+++ Day36_storeData/Day36_storeDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day36_storeDataApp.swift
+//  Day36_storeData
+//
+//  Created by Khalid Khan on 24/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Day36_storeDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
